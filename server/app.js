819078838008e2eb1e536e0e5792f23c30472188
Vishesh-Dev <developer.vishesh@gmail.com>
2019-02-12 22:18:19 +0530

    first commit

---
+++ server/app.js
@@ -0,0 +1,12 @@
+ const express = require('express');
+ const app=express();
+ const port=process.env.PORT || 3000;
+ const path = require('path');
+
+  app.use(express.static(path.join(__dirname,'../public')));
+
+
+
+ app.listen(port,()=>{
+   console.log(`Server is up and running on port ${port}`);
+ });
